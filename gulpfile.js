6852ded84d3d8041ec6b22c671f083023f3b4c67
phobos69 <svikeyoemmanuel@outlook.com>
2019-02-21 18:43:49 +0200

    2019 commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var nunjucksRender = require('gulp-nunjucks-render');
 
-gulp.task('nunjucks', function() {
-    // Gets .html and .nunjucks files in pages
-    return gulp.src('app/pages/**/*.+(html|nunjucks|njk)')
+sass.compiler = require('node-sass');
+
+gulp.task('nunjucks', function () {
+  // Gets .html and .nunjucks files in pages
+  return gulp.src('app/pages/**/*.+(html|nunjucks|njk)')
     // Renders template with nunjucks
     .pipe(nunjucksRender({
-        path: ['app/templates']
-      }))
+      path: ['app/templates']
+    }))
     // output files in app folder
     .pipe(gulp.dest('public'))
-  });
+});
+
+gulp.task('sass', function () {
+  return gulp.src('app/stylesheets/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('public/css'));
+});
